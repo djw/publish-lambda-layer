cc5ea12a06d7a4654bae7e1fcea6383634c3eff6
Dan Wilson <danjwilson@gmail.com>
2019-08-21 16:35:03 +0100

    First pass at layer upload action

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,43 @@
 import * as core from '@actions/core';
+import {Lambda, S3} from 'aws-sdk';
+import {readFile} from 'fs';
+
+const s3 = new S3({region: 'eu-west-1'});
+const lambda = new Lambda({region: 'eu-west-1'});
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
-    core.debug(`Hello ${myInput}`);
+    const path = core.getInput('path');
+    const bucket = core.getInput('bucket');
+    const key = core.getInput('key');
+    readFile(path, (err, buffer) => {
+      s3.putObject({
+        ACL: 'authenticated-read',
+        Body: buffer,
+        Bucket: bucket,
+        Key: key
+      }, (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          lambda.publishLayerVersion({
+            LayerName: core.getInput('layer'),
+            Description: core.getInput('description'),
+            Content: {
+              S3Bucket: bucket,
+              S3Key: key
+            },
+            CompatibleRuntimes: ['python3.7'],
+          }, (err, data) => {
+            if (err) {
+              throw err;
+            } else {
+              core.debug(JSON.stringify(data));
+            }
+          });
+        }
+      });
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
