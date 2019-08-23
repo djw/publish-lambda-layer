5a043d99d234d04933ec446c0fa44ea456b85b11
Dan Wilson <danjwilson@gmail.com>
2019-08-23 17:29:56 +0100

    Rewrite key to contain md5
    This should be behind an option, really.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
 import {Lambda, S3} from 'aws-sdk';
+import {createHash} from 'crypto';
 import {readFile} from 'fs';
+import {parse} from 'path';
 
 const s3 = new S3({region: 'eu-west-1'});
 const lambda = new Lambda({region: 'eu-west-1'});
@@ -9,8 +11,11 @@ async function run() {
   try {
     const path = core.getInput('path');
     const bucket = core.getInput('bucket');
-    const key = core.getInput('key');
+    const parsedKey = parse(core.getInput('key'));
+    const hash = createHash('md5');
     readFile(path, (err, buffer) => {
+      hash.update(buffer);
+      const key = `${parsedKey.dir}/${parsedKey.name}.${hash.digest('hex')}${parsedKey.ext}`;
       s3.putObject({
         ACL: 'authenticated-read',
         Body: buffer,
