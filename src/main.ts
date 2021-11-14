1b37a2f45aa7cb99a4e155fc1fa4a119e8e60718
Dan Wilson <work@danjwilson.co.uk>
2021-11-14 17:02:33 +0000

    Make architecture configurable
    Required aws-sdk update

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ async function run() {
               S3Key: key
             },
             CompatibleRuntimes: [core.getInput('runtime')],
+            CompatibleArchitectures: [core.getInput('architectures')]
           }, (err, data) => {
             if (err) {
               throw err;
